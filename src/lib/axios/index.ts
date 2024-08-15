77cb3e5546d2625479188c9b3c12da9c9681975f
Hinesh Miyani <hineshmiyani@gmail.com>
2024-08-15 23:33:20 +0530

    fix(axios): handle undefined error.response in interceptor

--- src/lib/axios/index.ts
+++ src/lib/axios/index.ts
@@ -29,7 +29,7 @@ api.interceptors.response.use(
   (error) => {
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
-    const { data } = error.response
+    const { data } = error.response || {}
 
     console.log(error.toJSON())
     return Promise.reject(data)
