e1f47420c5d41f6a2110764cf80980eee22d60d3
Hinesh Miyani <hineshmiyani@gmail.com>
2024-08-15 23:33:41 +0530

    refactor(ReactQueryProvider): initialize queryClient with useState

--- src/components/providers/ReactQueryProvider.tsx
+++ src/components/providers/ReactQueryProvider.tsx
@@ -1,12 +1,11 @@
 'use client'
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import React from 'react'
-
-export const queryClient = new QueryClient()
+import { useState } from 'react'
 
 const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(() => new QueryClient())
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
